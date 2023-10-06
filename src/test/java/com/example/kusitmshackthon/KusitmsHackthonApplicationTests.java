c5a1b8095bce90d65c27690f9896aabb2c314aac
RyuKwanKon <rhks33rhs@gmail.com>
2023-10-06 22:36:58 +0900

    [init] create project

---
+++ src/test/java/com/example/kusitmshackthon/KusitmsHackthonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kusitmshackthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusitmsHackthonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
