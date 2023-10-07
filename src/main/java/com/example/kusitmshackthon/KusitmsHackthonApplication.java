db867346480fa6b0a15cf1b30ffe9fd27778a208
versatile0010 <siam0294@naver.com>
2023-10-07 14:00:14 +0900

    chore: s3 set up

--- src/main/java/com/example/kusitmshackthon/KusitmsHackthonApplication.java
+++ src/main/java/com/example/kusitmshackthon/KusitmsHackthonApplication.java
@@ -1,13 +1,23 @@
 package com.example.kusitmshackthon;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class KusitmsHackthonApplication {
+    // aws sdk 연동
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KusitmsHackthonApplication.class, args);
     }
-
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
